6369bc992fc0bb1f563a2ce622a370de97177f38
akmlvl <armakmlvl@gmail.com>
2019-03-09 18:35:52 +0100

    Add files via upload

---
+++ WindowsApp7/Form1.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TwitchTheme1 = New TraderD.TwitchTheme()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TwitchLabel1 = New TraderD.TwitchLabel()
+        Me.RedemptionControlBox1 = New TraderD.RedemptionControlBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TwitchTheme1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TwitchTheme1
+        '
+        Me.TwitchTheme1.BackColor = System.Drawing.Color.White
+        Me.TwitchTheme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.TwitchTheme1.Controls.Add(Me.Label3)
+        Me.TwitchTheme1.Controls.Add(Me.Label2)
+        Me.TwitchTheme1.Controls.Add(Me.Label1)
+        Me.TwitchTheme1.Controls.Add(Me.TwitchLabel1)
+        Me.TwitchTheme1.Controls.Add(Me.RedemptionControlBox1)
+        Me.TwitchTheme1.Controls.Add(Me.Panel1)
+        Me.TwitchTheme1.Customization = "AAD//w=="
+        Me.TwitchTheme1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TwitchTheme1.Fill = True
+        Me.TwitchTheme1.Font = New System.Drawing.Font("Leelawadee UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TwitchTheme1.Image = Nothing
+        Me.TwitchTheme1.Location = New System.Drawing.Point(0, 0)
+        Me.TwitchTheme1.Margin = New System.Windows.Forms.Padding(2)
+        Me.TwitchTheme1.Movable = True
+        Me.TwitchTheme1.Name = "TwitchTheme1"
+        Me.TwitchTheme1.NoRounding = False
+        Me.TwitchTheme1.Sizable = True
+        Me.TwitchTheme1.Size = New System.Drawing.Size(711, 465)
+        Me.TwitchTheme1.SmartBounds = True
+        Me.TwitchTheme1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation
+        Me.TwitchTheme1.TabIndex = 0
+        Me.TwitchTheme1.Text = "zaza"
+        Me.TwitchTheme1.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.TwitchTheme1.Transparent = False
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label3.Font = New System.Drawing.Font("Trebuchet MS", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(3, 436)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(27, 29)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "+"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Leelawadee UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(628, 439)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "By Akmlvl"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(11, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "TraderD"
+        '
+        'TwitchLabel1
+        '
+        Me.TwitchLabel1.AutoSize = True
+        Me.TwitchLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.TwitchLabel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.TwitchLabel1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.TwitchLabel1.Location = New System.Drawing.Point(82, 11)
+        Me.TwitchLabel1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.TwitchLabel1.Name = "TwitchLabel1"
+        Me.TwitchLabel1.Size = New System.Drawing.Size(25, 23)
+        Me.TwitchLabel1.TabIndex = 2
+        Me.TwitchLabel1.Text = "⬤"
+        '
+        'RedemptionControlBox1
+        '
+        Me.RedemptionControlBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RedemptionControlBox1.BackColor = System.Drawing.Color.Transparent
+        Me.RedemptionControlBox1.ForeColor = System.Drawing.Color.White
+        Me.RedemptionControlBox1.Location = New System.Drawing.Point(641, 5)
+        Me.RedemptionControlBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.RedemptionControlBox1.Name = "RedemptionControlBox1"
+        Me.RedemptionControlBox1.Size = New System.Drawing.Size(60, 25)
+        Me.RedemptionControlBox1.TabIndex = 1
+        Me.RedemptionControlBox1.Text = "0"
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(19, Byte), Integer), CType(CType(23, Byte), Integer), CType(CType(34, Byte), Integer))
+        Me.Panel1.Location = New System.Drawing.Point(5, 46)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(702, 389)
+        Me.Panel1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(711, 465)
+        Me.Controls.Add(Me.TwitchTheme1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.TwitchTheme1.ResumeLayout(False)
+        Me.TwitchTheme1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TwitchTheme1 As TwitchTheme
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents RedemptionControlBox1 As RedemptionControlBox
+    Friend WithEvents TwitchLabel1 As TwitchLabel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ WindowsApp7/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs)
+    End Sub
+    Dim curwidh As Integer = Me.Width
+    Dim curheight As Integer = Me.Height
+    Dim path = Application.StartupPath + "/BTCUSD.HTML"
+
+    Dim browser = New CefSharp.WinForms.ChromiumWebBrowser(path) With {.Dock = DockStyle.Fill, .Padding = New Padding(0, 0, 0, 0)}
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Panel1.AllowDrop = True
+        Panel1.Controls.Add(browser)
+        'D:\TraderD\xrp.html
+        Me.KeyPreview = True
+
+    End Sub
+    Private Sub Form1_DragDrop(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles Me.DragDrop
+
+    End Sub
+
+    Private Sub TwitchLabel1_Click(sender As Object, e As EventArgs) Handles TwitchLabel1.Click
+        CefSharp.WebBrowserExtensions.Reload(browser)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        Dim f As New Form1
+        f.Show()
+    End Sub
+End Class
